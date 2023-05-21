0d0283ce4be5d38a600d334772bcbdda72a8ceb3
Miquelane Melo <miquelanepmelo@gmail.com>
2023-05-21 11:45:10 -0300

    first commit

---
+++ adivinhacao.py
@@ -0,0 +1,32 @@
+import random
+from unicodedata import numeric
+
+print("********************************")
+print("Bem vindo ao jogo de adivinhação!")
+print("********************************")
+
+numero_secreto =  random.randrange(1,101)
+total_tentativas = 3
+rodada = 1
+
+print(numero_secreto)
+
+for rodada in range (1, total_tentativas + 1):
+    print("Tentativa: {} de {}".format(rodada , total_tentativas))
+    chute = input("Digite o seu número: ")
+    print("Você digitou ", chute)
+    numero = int(chute)
+
+    if numero_secreto == numero:
+        print("Você acertou")
+        break
+    else:
+        if(numero > numero_secreto):
+            print("Você errou! O seu chute foi maior que o numero correto")
+        elif(numero < numero_secreto):
+            print("Você errou! O seu chute foi menor que o numero correto")
+
+    rodada = rodada + 1
+
+print("Fim do jogo!")
+
