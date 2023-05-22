e0a06b68fa52a2a5a28630c8219a9394c3689da1
Miquelane Melo <miquelanepmelo@gmail.com>
2023-05-22 09:44:07 -0300

    implementando lógica para jofo forca

--- forca.py
+++ forca.py
@@ -4,4 +4,22 @@ def jogar_forca():
     print("Bem vindo ao jogo da forca!")
     print("********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        chute = input("Qual Letra? ")
+
+        index = 0
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
+        print("Jogando....")
+
+
     print("Fim de Jogo!")
+
+if(__name__ == "__main__"):
+    jogar_forca()
